24fb4014be01eda4c05f749bad065efe4d712fba
nikistefanov <nikistefanovbg89@gmail.com>
2017-02-04 00:40:32 +0200

    Added some tests

--- das-boot/src/main/java/com/boot/controller/HomeController.java
+++ das-boot/src/main/java/com/boot/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "<script>window.location.href = 'index.html';</script>Please enable your javascript...";
+		return "<script>window.location.href = 'index.html';</script><h1>Please enable your javascript...</h1>";
 	}
 }
